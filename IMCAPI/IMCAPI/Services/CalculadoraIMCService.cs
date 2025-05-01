4e88aeb92b5ba6a5f6fd424adc76e057cfe3af52
David04eng <djem12032004@gmail.com>
2025-04-30 23:44:07 -0600

    Archivo CalculadoraIMCService.cs cambio linea 16, no se porque era el error pero ya estufas

--- IMCAPI/IMCAPI/Services/CalculadoraIMCService.cs
+++ IMCAPI/IMCAPI/Services/CalculadoraIMCService.cs
@@ -1,25 +1,28 @@
-﻿namespace IMCAPI.Services;
+﻿using System;
 
-public class CalculadoraIMCService
+namespace IMCAPI.Services
 {
-    public (double imc, string categoria) CalcularIMC(double peso, double alturaEnMetros) // Espera altura en metros
+    public class CalculadoraIMCService
     {
-        if (peso <= 0 || alturaEnMetros <= 0)
-            throw new ArgumentException("Peso y altura deben ser positivos.");
-
-        double imc = peso / (alturaEnMetros * alturaEnMetros);
-
-        string categoria = imc switch
+        public (double imc, string categoria) CalcularIMC(double peso, double alturaEnMetros)
         {
-            < 18.5 => "Muy Bajo peso",
-            >= 18.5 and < 25 => "Normal",
-            >= 25 and < 30 => "Sobrepeso",
-            >= 30 and < 35 => "Obesidad grado I",
-            >= 35 and < 40 => "Obesidad grado II",
-            >= 40 => "Obesidad grado III",
-            _ => "No clasificado"
-        };
+            if (peso <= 0 || alturaEnMetros <= 0)
+                throw new ArgumentException("Peso y altura deben ser positivos.");
 
-        return (imc, categoria);
+            double imc = peso / (alturaEnMetros * alturaEnMetros);
+
+            string categoria = imc switch
+            {
+                < 18.5 => "Bajo peso",                  // <-- Aquí estaba el problema
+                >= 18.5 and < 25 => "Normal",
+                >= 25 and < 30 => "Sobrepeso",
+                >= 30 and < 35 => "Obesidad grado I",
+                >= 35 and < 40 => "Obesidad grado II",
+                >= 40 => "Obesidad grado III",
+                _ => "No clasificado"
+            };
+
+            return (imc, categoria);
+        }
     }
 }
